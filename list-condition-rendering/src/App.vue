e60070544a41cd2b5f108980e16574690619ad3c
vadym-zinchenko-moc <vadym.zinchenko@masterofcode.com>
2022-12-04 23:19:49 +0200

    list_conditions-rendering

---
+++ list-condition-rendering/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="best-films-actors">
+    <BestFilms v-if="isShow" />
+    <BestActor v-else />
+    <button @click="toggleShow" class="toggle-button">Toggle show</button>
+  </div>
+</template>
+
+<script>
+import BestActor from "./components/BestActor";
+import BestFilms from "./components/BestFilms";
+
+export default {
+  name: "App",
+  components: { BestActor, BestFilms },
+  data: () => ({
+    isShow: true,
+  }),
+  methods:{ 
+    toggleShow() {
+      this.isShow = !this.isShow
+    }
+  }
+};
+</script>
+
+<style scoped>
+.best-films-actors {
+  margin: 0 auto;
+  max-width: 300px;
+}
+</style>
---
+++ list-condition-rendering/src/components/BestFilms.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="top-films">
+    <h1 class="top-films__title">Top films</h1>
+    <ul v-if="filmsCount" class="top-films__items">
+      <li v-for="film in films" :key="film.id" class="top-films__item">
+        {{ film.Title }}
+      </li>
+    </ul>
+    <p v-else class="top-films__empty">Film list is empty</p>
+  </div>
+</template>
+
+<script>
+import films from "../data.js";
+
+export default {
+  name: "BestFilms",
+  data: () => ({
+    films,
+  }),
+  computed: {
+    filmsCount() {
+      return this.films.length;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.top-films__title {
+  font-size: 42px;
+  font-weight: 700;
+  color: #430e0e;
+}
+
+.top-films__item {
+  font-size: 16px;
+}
+</style>
