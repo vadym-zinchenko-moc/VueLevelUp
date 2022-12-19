c7c7c0678050d8b2eedf438fbcfcb2f2dc84cf14
vadym-zinchenko-moc <vadym.zinchenko@masterofcode.com>
2022-12-19 12:56:30 +0200

    style_scoped_data-binding

---
+++ styles_scoped_data-binding/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1 class="test">Test</h1>
+    <h1 class="test-one">Test-one</h1>
+    <h1 class="test-two">Test-two</h1>
+    <h1 class="test-three">Test-three</h1>
+    <h1 class="bind-style">Bind style</h1>
+    <button @click="changeColor">Change bind color</button>
+    <h1 :class="classes.yellow">Test module</h1>
+    <h1 :class="$style.module">Module</h1>
+    <TestComponent />
+  </div>
+</template>
+
+<script>
+import TestComponent from "./components/TestComponent";
+
+export default {
+  name: "App",
+  components: { TestComponent },
+  data: () => ({
+    color: "blue",
+  }),
+  methods: {
+    changeColor() {
+      this.color = "pink";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.test {
+  color: rgb(230, 12, 12);
+}
+
+.bind-style {
+  color: v-bind(color);
+}
+
+:global(.test-one) {
+  color: #cecdcd;
+}
+</style>
+
+<style module="classes">
+.yellow {
+  color: rgb(231, 231, 24);
+}
+</style>
+
+<style>
+.test-two {
+  color: rgb(11, 118, 11);
+}
+</style>
+
+<style module>
+.module {
+  color: rgb(127, 26, 222);
+}
+</style>
---
+++ styles_scoped_data-binding/src/components/TestComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 class="test">Test-child</h1>
+    <h1 class="test-one">Test-child-one</h1>
+    <h1 class="test-two">Test-child-two</h1>
+    <h1 class="test-three">Test-child-three</h1>
+    <h1 class="setup-script">Setup</h1>
+  </div>
+</template>
+
+<script setup>
+const theme = {
+  color: "brown",
+};
+</script>
+
+<script>
+export default {
+  name: "TestComponent",
+};
+</script>
+
+<style>
+.test-three {
+  color: rgb(0, 255, 255);
+}
+
+.setup-script {
+  color: v-bind("theme.color");
+}
+</style>
